300e7d3f743705cea5e7e121eda4d451ef269315
Borja Esteban <boressan@outlook.com>
2024-07-23 15:58:53 +0200

    add POST experiment tests for 422 response

---
+++ tests/blp_experiment/post/test_422.py
@@ -0,0 +1,56 @@
+"""Testing module for endpoint methods /experiment."""
+
+# pylint: disable=redefined-outer-name
+from pytest import mark
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class CommonBaseTests:
+    """Common tests for the /experiment endpoint."""
+
+    def test_status_code(self, response):
+        """Test the 422 response."""
+        assert response.status_code == 422
+
+
+class UnknownField:
+    """Test response message contains unknown field."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"].values()
+        assert ["Unknown field."] in errors
+
+
+class InvalidString:
+    """Test response message contains invalid field."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"].values()
+        assert ["Not a valid string."] in errors
+
+
+class InvalidList:
+    """Test response message contains invalid list."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"].values()
+        assert ["Not a valid list."] in errors
+
+
+@mark.parametrize("body", [{"bad_key": "val"}], indirect=True)
+class TestBadBodyKey(UnknownField, CommonBaseTests):
+    """Test the bad_key parameter in the body."""
+
+
+@mark.parametrize("name", [1000], indirect=True)
+class TestBadExperiment(InvalidString, CommonBaseTests):
+    """Test experiment name is not a string."""
+
+
+@mark.parametrize("permissions", ["non_list"], indirect=True)
+class TestBadPermissions(InvalidList, CommonBaseTests):
+    """Test permissions is not a list."""
