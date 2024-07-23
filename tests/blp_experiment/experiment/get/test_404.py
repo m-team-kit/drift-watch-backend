d260ea887c24fc7231b873ff1c41eb455bfb1642
Borja Esteban <boressan@outlook.com>
2024-07-23 16:37:09 +0200

    add GET experiment tests for response 404

---
+++ tests/blp_experiment/experiment/get/test_404.py
@@ -0,0 +1,30 @@
+"""Testing module for endpoint methods /experiment."""
+
+# pylint: disable=redefined-outer-name
+from pytest import mark
+
+
+@mark.parametrize("auth", [None], indirect=True)
+class CommonBaseTests:
+    """Common tests for the /experiment endpoint."""
+
+    def test_status_code(self, response):
+        """Test the 404 response."""
+        assert response.status_code == 404
+
+
+class NotFound:
+    """Test the when experiment Id is not in database."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["code"] == 404
+        assert response.json["status"] == "Not Found"
+
+
+EXPERIMENT_X = "00000000-0000-0001-0001-999999999999"
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_X], indirect=True)
+class TestNotInDatabase(NotFound, CommonBaseTests):
+    """Test the when experiment Id is not in database."""
