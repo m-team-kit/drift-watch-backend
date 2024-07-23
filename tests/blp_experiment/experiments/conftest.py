56e4186c95c3fa02ca6887f9fc0ed3726936aa22
Borja Esteban <boressan@outlook.com>
2024-07-23 16:05:51 +0200

    shift test structure to follow module and class dev

---
+++ tests/blp_experiment/experiments/conftest.py
@@ -0,0 +1,16 @@
+"""Testing module for endpoint methods /experiment."""
+
+# pylint: disable=redefined-outer-name
+from pytest import fixture
+
+
+@fixture(scope="module")
+def path(request):
+    """Return the path for the request."""
+    return request.param if hasattr(request, "param") else "/experiment"
+
+
+@fixture(scope="module")
+def collection(database):
+    """Return a collection connection to the database."""
+    return database["app.experiments"]
