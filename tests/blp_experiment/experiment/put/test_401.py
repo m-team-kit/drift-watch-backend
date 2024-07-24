4f8c5ed524c860d4452d7b9bc637b4ac443fa64b
Borja Esteban <boressan@outlook.com>
2024-07-24 13:10:13 +0200

    add PUT experiments tests for response 401

---
+++ tests/blp_experiment/experiment/put/test_401.py
@@ -0,0 +1,49 @@
+"""Testing module for endpoint methods /experiment."""
+
+# pylint: disable=redefined-outer-name
+from pytest import mark
+
+
+@mark.parametrize("name", ["experiment_a"], indirect=True)
+@mark.parametrize("permissions", [[]], indirect=True)
+class CommonBaseTests:
+    """Common tests for the /experiment endpoint."""
+
+    def test_status_code(self, response):
+        """Test the 401 response."""
+        assert response.status_code == 401
+
+
+class NoAuthHeader:
+    """Tests when missing authentication header."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["error"] == "Unauthenticated"
+        description = response.json["error_description"]
+        assert description == "No authorization header"
+
+
+class UnknownIdentity:
+    """Test when identity provided is unknown."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["error"] == "Unauthenticated"
+        description = response.json["error_description"]
+        assert description == "User identity could not be determined"
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+
+
+@mark.parametrize("auth", [None], indirect=True)
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+class TestMissingToken(NoAuthHeader, CommonBaseTests):
+    """Test the /experiment endpoint with missing token."""
+
+
+@mark.parametrize("auth", ["invalid_token"], indirect=True)
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+class TestInvalidToken(UnknownIdentity, CommonBaseTests):
+    """Test the /experiment endpoint with invalid token."""
