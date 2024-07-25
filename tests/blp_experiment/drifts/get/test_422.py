3004f40ebfab4569bacc2d389127675ea49d6425
Borja Esteban <boressan@outlook.com>
2024-07-25 12:17:22 +0200

    add GET drift tests for 422 response

--- tests/blp_experiment/drifts/get/test_422.py
+++ tests/blp_experiment/drifts/get/test_422.py
@@ -1,25 +1,27 @@
 """Testing module for endpoint methods /drift."""
 
 # pylint: disable=redefined-outer-name
-from pytest import mark, fixture
+from pytest import mark
 
 
-@mark.parametrize("auth", [None], indirect=True)
-class CommonTests:
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+@mark.usefixtures("accept_authorization")
+class CommonBaseTests:
     """Common tests for the /drift endpoint."""
-
-    @fixture(scope="class")
-    def body(self, request):
-        """Inject and return a request body."""
-        return request.param
 
     def test_status_code(self, response):
         """Test the 422 response."""
         assert response.status_code == 422
+        assert response.json["code"] == 422
 
 
-@mark.parametrize("body", ["string_body"], indirect=True)
-class TestBadBody(CommonTests):
+class ErrorMessage:
     """Test the bad_key parameter."""
 
     def test_error_msg(self, response):
@@ -27,3 +29,8 @@ class TestBadBody(CommonTests):
         assert "_schema" in response.json["errors"]["json"]
         error = response.json["errors"]["json"]["_schema"]
         assert error == ["Invalid input type."]
+
+
+@mark.parametrize("body", ["string_body"], indirect=True)
+class TestStringBody(ErrorMessage, CommonBaseTests):
+    """Test the response when body is a string."""
