7eacdb38279da14b682269b84d4cf821e7ee1b2e
Borja Esteban <boressan@outlook.com>
2024-07-26 09:39:14 +0200

    fix GET drift tests for 404 response

--- tests/blp_experiment/drift/get/test_404.py
+++ tests/blp_experiment/drift/get/test_404.py
@@ -4,30 +4,51 @@
 from pytest import mark
 
 
-@mark.parametrize("auth", [None], indirect=True)
-class CommonTests:
-    """Common tests for the /drift endpoint."""
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+@mark.usefixtures("accept_authorization")
+class CommonBaseTests:
+    """Common tests for the /drift/<id> endpoint."""
 
     def test_status_code(self, response):
         """Test the 404 response."""
         assert response.status_code == 404
+        assert response.json["code"] == 404
 
 
-@mark.parametrize(
-    "drift_id",  # Parametrize by class to cluster tests in same group
-    [  # List of drift ids to test in this class
-        "4401dde0-32f2-4db3-8154-6cd88764f699",
-    ],
-    indirect=True,
-)
-class DriftV100(CommonTests):
-    """Tests for V100 drift ids."""
+class ExperimentNotFound:
+    """Test the when experiment Id is not in database."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["status"] == "Not Found"
+        assert response.json["message"] == "Experiment not found."
 
 
-class TestNotFound(DriftV100):
+class DriftNotFound:
     """Test the when drift Id is not in database."""
 
-    def test_error(self, response):
+    def test_error_msg(self, response):
         """Test message contains useful information."""
-        assert response.json["code"] == 404
         assert response.json["status"] == "Not Found"
+        assert response.json["message"] == "Drift not found."
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+EXPERIMENT_X = "00000000-0000-0001-0001-999999999999"
+
+DRIFT_EXP1_X = "00000000-0000-0004-0001-999999999999"
+DRIFT_EXPX_X = "00000000-0000-0004-9999-999999999999"
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_X], indirect=True)
+@mark.parametrize("drift_id", [DRIFT_EXPX_X], indirect=True)
+class TestExperimentNotInDB(ExperimentNotFound, CommonBaseTests):
+    """Test the when experiment Id is not in database."""
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+@mark.parametrize("drift_id", [DRIFT_EXP1_X], indirect=True)
+class TestDriftNotInDB(DriftNotFound, CommonBaseTests):
+    """Test the when drift Id is not in database."""
