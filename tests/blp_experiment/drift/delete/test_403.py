1cf5aa8cfe87a19ab34bdbb20892d62f5976b084
Borja Esteban <boressan@outlook.com>
2024-07-26 10:35:46 +0200

    fix DELETE drift tests for 403 response

--- tests/blp_experiment/drift/delete/test_403.py
+++ tests/blp_experiment/drift/delete/test_403.py
@@ -4,54 +4,56 @@
 from pytest import mark
 
 
-class CommonTests:
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+@mark.usefixtures("accept_authorization")
+class CommonBaseTests:
     """Common tests for the /drift endpoint."""
 
     def test_status_code(self, response):
         """Test the 403 response."""
         assert response.status_code == 403
+        assert response.json["code"] == 403
+
+    def test_in_database(self, db_drift):
+        """Test the response items are in the database."""
+        assert db_drift is not None
 
 
-@mark.parametrize(
-    "drift_id",  # Parametrize by class to cluster tests in same group
-    [  # List of drift ids to test in this class
-        "00000000-0000-0001-0001-000000000001",
-    ],
-    indirect=True,
-)
-class DriftV100(CommonTests):
-    """Tests for V100 drift ids."""
-
-
-@mark.parametrize("auth", ["invalid_token"], indirect=True)
-@mark.usefixtures("accept_authorization")
-class TestNotRegistered(DriftV100):
-    """Test response when user is not registered."""
+class NotRegistered:
+    """Tests for message response when user is not registered."""
 
     def test_error_msg(self, response):
         """Test message contains useful information."""
         assert response.json["status"] == "Forbidden"
-        description = response.json["error_description"]
-        assert description == "User user_1@issuer_1 does not meet requirements"
+        assert response.json["message"] == "User not registered."
 
 
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.parametrize(
-    "drift_id",  # Parametrize by class to cluster tests in same group
-    [  # List of drift ids to test in this class
-        "00000000-0000-0001-0001-000000000002",
-        "4401dde0-32f2-4db3-8154-6cd88764f699",
-    ],
-    indirect=True,
-)
-@mark.parametrize("with_database", ["database_1"], indirect=True)
-@mark.usefixtures("with_context", "with_database")
-@mark.usefixtures("accept_authorization")
-class TestNotOwned(CommonTests):
-    """Test response when drift does not belong to owner."""
+class PermissionDenied:
+    """Tests for message response when user does not have permission."""
 
     def test_error_msg(self, response):
         """Test message contains useful information."""
         assert response.json["status"] == "Forbidden"
-        description = response.json["error_description"]
-        assert description == "User user_1@issuer_1 does not meet requirements"
+        assert response.json["message"] == "Insufficient permissions."
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+EXPERIMENT_2 = "00000000-0000-0001-0001-000000000002"
+
+DRIFT_EXP1_1 = "00000000-0000-0004-0001-000000000001"
+DRIFT_EXP2_1 = "00000000-0000-0004-0002-000000000001"
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+@mark.parametrize("drift_id", [DRIFT_EXP1_1], indirect=True)
+@mark.parametrize("subiss", [("unknown", "egi.com")], indirect=True)
+class TestNotRegistered(NotRegistered, CommonBaseTests):
+    """Test the authentication response when user not registered."""
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
+@mark.parametrize("drift_id", [DRIFT_EXP2_1], indirect=True)
+class TestPermission(PermissionDenied, CommonBaseTests):
+    """Tests for message response when user does not have manage permission."""
