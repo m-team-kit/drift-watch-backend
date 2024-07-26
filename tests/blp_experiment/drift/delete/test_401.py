5779fb514e0a52be3cd82e43fd15a0bc83fd68a4
Borja Esteban <boressan@outlook.com>
2024-07-26 10:35:38 +0200

    fix DELETE drift tests for 401 response

--- tests/blp_experiment/drift/delete/test_401.py
+++ tests/blp_experiment/drift/delete/test_401.py
@@ -3,43 +3,53 @@
 # pylint: disable=redefined-outer-name
 from pytest import mark
 
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
 
-class CommonTests:
+
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class CommonBaseTests:
     """Common tests for the /drift endpoint."""
 
     def test_status_code(self, response):
         """Test the 401 response."""
         assert response.status_code == 401
+        assert response.json["code"] == 401
+
+    def test_in_database(self, db_drift):
+        """Test the response items are in the database."""
+        assert db_drift is not None
 
 
-@mark.parametrize(
-    "drift_id",  # Parametrize by class to cluster tests in same group
-    [  # List of drift ids to test in this class
-        "00000000-0000-0001-0001-000000000001",
-    ],
-    indirect=True,
-)
-class DriftV100(CommonTests):
-    """Tests for V100 drift ids."""
+class NoAuthHeader:
+    """Tests when missing authentication header."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["status"] == "Unauthorized"
+        assert response.json["message"] == "No authorization header"
 
 
+class UnknownIdentity:
+    """Test when identity provided is unknown."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["status"] == "Unauthorized"
+        assert response.json["message"] == "User identity could not be determined"
+
+
+DRIFT_V100_1 = "00000000-0000-0004-0001-000000000001"
+
+
+@mark.parametrize("drift_id", [DRIFT_V100_1], indirect=True)
 @mark.parametrize("auth", [None], indirect=True)
-class TestMissingToken(DriftV100):
-    """Test the bad_key parameter."""
-
-    def test_error_msg(self, response):
-        """Test message contains useful information."""
-        assert response.json["status"] == "Unauthenticated"
-        description = response.json["error_description"]
-        assert description == "No authorization header"
+class TestMissingToken(NoAuthHeader, CommonBaseTests):
+    """Test the /experiment endpoint with missing token."""
 
 
+@mark.parametrize("drift_id", [DRIFT_V100_1], indirect=True)
 @mark.parametrize("auth", ["invalid_token"], indirect=True)
-class TestInvalidToken(DriftV100):
-    """Test the bad_key parameter."""
-
-    def test_error_msg(self, response):
-        """Test message contains useful information."""
-        assert response.json["status"] == "Unauthenticated"
-        description = response.json["error_description"]
-        assert description == "User identity could not be determined"
+class TestInvalidToken(UnknownIdentity, CommonBaseTests):
+    """Test the /experiment endpoint with invalid token."""
