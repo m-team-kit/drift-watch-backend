828cda6d7b5b87bc78f62f9e1bbd1d4e96142cfd
Borja Esteban <boressan@outlook.com>
2024-07-26 08:35:20 +0200

    fix POST drift tests for 403 response

--- tests/blp_experiment/drifts/post/test_403.py
+++ tests/blp_experiment/drifts/post/test_403.py
@@ -4,27 +4,47 @@
 from pytest import mark
 
 
-@mark.parametrize("job_status", ["Running"], indirect=True)
-@mark.parametrize("model", ["example_1"], indirect=True)
-@mark.parametrize("concept_drift", [None], indirect=True)
-@mark.parametrize("data_drift", [None], indirect=True)
-class CommonTests:
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+@mark.usefixtures("accept_authorization")
+class CommonBaseTests:
     """Common tests for the /drift endpoint."""
 
     def test_status_code(self, response):
         """Test the 403 response."""
         assert response.status_code == 403
+        assert response.json["code"] == 403
 
 
-@mark.parametrize("auth", ["invalid_token"], indirect=True)
-@mark.usefixtures("accept_authorization")
-class TestNotRegistered(CommonTests):
-    """Test the bad_key parameter."""
+class NotRegistered:
+    """Tests for message response when user is not registered."""
 
     def test_error_msg(self, response):
         """Test message contains useful information."""
         assert response.json["status"] == "Forbidden"
-        description = response.json["error_description"]
-        assert description == "User user_1@issuer_1 does not meet requirements"
-        details = response.json["error_details"]
-        assert details == "Evaluation of: is_registered"
+        assert response.json["message"] == "User not registered."
+
+
+class PermissionDenied:
+    """Tests for message response when user does not have permission."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        assert response.json["status"] == "Forbidden"
+        assert response.json["message"] == "Insufficient permissions."
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+EXPERIMENT_2 = "00000000-0000-0001-0001-000000000002"
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+@mark.parametrize("subiss", [("unknown", "egi.com")], indirect=True)
+class TestNotRegistered(NotRegistered, CommonBaseTests):
+    """Test the authentication response when user not registered."""
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
+class TestPermission(PermissionDenied, CommonBaseTests):
+    """Tests for message response when user does not have manage permission."""
