7d1df9425303b6ad3d570329347985c61d009a30
Borja Esteban <boressan@outlook.com>
2024-07-29 15:53:19 +0200

    improve GET users tests for response 422

--- tests/blp_user/get/test_422.py
+++ tests/blp_user/get/test_422.py
@@ -1,27 +1,21 @@
-"""Testing module for endpoint methods /drift."""
+"""Testing module for endpoint methods /user."""
 
 # pylint: disable=redefined-outer-name
-from pytest import mark, fixture
+from pytest import mark
 
 
 @mark.parametrize("auth", ["mock-token"], indirect=True)
 @mark.parametrize("entitlements", [["iam:admin"]], indirect=True)
 @mark.usefixtures("accept_authorization")
-class CommonTests:
-    """Common tests for the /drift endpoint."""
-
-    @fixture(scope="class")
-    def body(self, request):
-        """Inject and return a request body."""
-        return request.param
+class CommonBaseTests:
+    """Common tests for the /user endpoint."""
 
     def test_status_code(self, response):
         """Test the 422 response."""
         assert response.status_code == 422
 
 
-@mark.parametrize("body", ["string_body"], indirect=True)
-class TestBadBody(CommonTests):
+class InvalidInput:
     """Test the bad_key parameter."""
 
     def test_error_msg(self, response):
@@ -29,3 +23,8 @@ class TestBadBody(CommonTests):
         assert "_schema" in response.json["errors"]["json"]
         error = response.json["errors"]["json"]["_schema"]
         assert error == ["Invalid input type."]
+
+
+@mark.parametrize("body", ["string_body"], indirect=True)
+class TestStringBody(InvalidInput, CommonBaseTests):
+    """Test the response when body is a string."""
