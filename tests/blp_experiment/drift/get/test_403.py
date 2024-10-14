aa418ea490078c95dbaceac739c19045937b27dc
Borja Esteban <boressan@outlook.com>
2024-10-14 13:04:11 +0000

    fix tests drift get 403

--- tests/blp_experiment/drift/get/test_403.py
+++ tests/blp_experiment/drift/get/test_403.py
@@ -4,10 +4,6 @@
 from pytest import mark
 
 
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.parametrize("with_database", ["database_1"], indirect=True)
-@mark.usefixtures("with_context", "with_database")
-@mark.usefixtures("accept_authorization")
 class CommonBaseTests:
     """Common tests for the /drift endpoint."""
 
@@ -17,7 +13,24 @@ class CommonBaseTests:
         assert response.json["code"] == 403
 
 
-class NotRegistered:
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
+
+    def test_in_database(self, db_drift):
+        """Test the response items are in the database."""
+        assert db_drift is not None
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
+
+
+@mark.parametrize("subiss", [("unknown", "issuer.1")], indirect=True)
+class NotRegistered(ValidAuth):
     """Tests for message response when user is not registered."""
 
     def test_error_msg(self, response):
@@ -26,7 +39,17 @@ class NotRegistered:
         assert response.json["message"] == "User not registered."
 
 
-class PermissionDenied:
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+EXPERIMENT_2 = "00000000-0000-0001-0001-000000000002"
+ENT_READ = "urn:mace:egi.eu:group:vo_example1:role=read#x.0"
+
+
+class PermissionDenied(Registered):
     """Tests for message response when user does not have permission."""
 
     def test_error_msg(self, response):
@@ -35,21 +58,29 @@ class PermissionDenied:
         assert response.json["message"] == "Insufficient permissions."
 
 
-EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
-EXPERIMENT_2 = "00000000-0000-0001-0001-000000000002"
-
-DRIFT_EXP1_1 = "00000000-0000-0000-0000-000000000001"
-DRIFT_EXP2_1 = "00000000-0000-0000-0000-000000000001"
-
-
 @mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("drift_id", [DRIFT_EXP1_1], indirect=True)
-@mark.parametrize("subiss", [("unknown", "egi.com")], indirect=True)
-class TestNotRegistered(NotRegistered, CommonBaseTests):
-    """Test the authentication response when user not registered."""
+class IsPrivate(CommonBaseTests):
+    """Base class for group with public as false."""
 
 
 @mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
-@mark.parametrize("drift_id", [DRIFT_EXP2_1], indirect=True)
-class TestPermission(PermissionDenied, CommonBaseTests):
-    """Tests for message response when user does not have manage permission."""
+class IsPublic(CommonBaseTests):
+    """Base class for group with public as true."""
+
+
+@mark.parametrize("entitlements", [[]], indirect=True)
+class NoGroup(IsPrivate):
+    """Base class for group without entitlement tests."""
+
+
+DRIFT_1 = "00000000-0000-0000-0000-000000000001"
+
+
+@mark.parametrize("drift_id", [DRIFT_1], indirect=True)
+class TestNotRegistered(NotRegistered, IsPublic, WithDatabase):
+    """Test the authentication response when user not registered."""
+
+
+@mark.parametrize("drift_id", [DRIFT_1], indirect=True)
+class TestNoAccess(PermissionDenied, NoGroup, WithDatabase):
+    """Tests for message response for no permission."""
