7e882b827638278f085a4837275d42bb07c17833
Borja Esteban <boressan@outlook.com>
2024-10-15 11:46:06 +0000

    fix tests drifts post 422 response

--- tests/blp_experiment/drifts/post/test_422.py
+++ tests/blp_experiment/drifts/post/test_422.py
@@ -1,19 +1,11 @@
-"""Testing module for endpoint methods /drift."""
+"""Testing module for endpoint methods /drifts."""
 
 # pylint: disable=redefined-outer-name
 from pytest import mark
 
 
-EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
-
-
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("with_database", ["database_1"], indirect=True)
-@mark.usefixtures("with_context", "with_database")
-@mark.usefixtures("accept_authorization")
 class CommonBaseTests:
-    """Common tests for the /drift endpoint."""
+    """Common tests for the /drifts endpoint."""
 
     def test_status_code(self, response):
         """Test the 422 response."""
@@ -21,7 +13,43 @@ class CommonBaseTests:
         assert response.json["code"] == 422
 
 
-class UnknownField:
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
+
+
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
+
+
+EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
+class IsPrivate(CommonBaseTests):
+    """Base class for group with public as false."""
+
+
+ENT_MANAGE = "urn:mace:egi.eu:group:vo_example1:role=manage#x.0"
+ENT_EDIT = "urn:mace:egi.eu:group:vo_example1:role=edit#x.0"
+ENT_READ = "urn:mace:egi.eu:group:vo_example1:role=read#x.0"
+
+
+@mark.parametrize("entitlements", [[ENT_EDIT]], indirect=True)
+class EditGroup(Registered):
+    """Base class for group with manage entitlement tests."""
+
+
+@mark.parametrize("body", [{"unknown": "val"}], indirect=True)
+class UnknownField(CommonBaseTests):
     """Test the response message for unknown key in body."""
 
     def test_error_msg(self, response):
@@ -30,7 +58,12 @@ class UnknownField:
         assert ["Unknown field."] in errors
 
 
-class NoBoolDrift:
+NO_BOOL_CD_BODY = {"concept_drift": {"drift": "a", "parameters": {"n": 0}}}
+NO_BOOL_DD_BODY = {"data_drift": {"drift": "a", "parameters": {"n": 0}}}
+
+
+@mark.parametrize("body", [NO_BOOL_CD_BODY, NO_BOOL_DD_BODY], indirect=True)
+class NoBoolDrift(CommonBaseTests):
     """Test the response message for missing drift boolean."""
 
     def test_error_msg(self, response):
@@ -39,7 +72,12 @@ class NoBoolDrift:
         assert {"drift": ["Not a valid boolean."]} in errors
 
 
-class MissingParam:
+NO_PRAM_CD_BODY = {"concept_drift": {"drift": True}}
+NO_PRAM_DD_BODY = {"data_drift": {"drift": True}}
+
+
+@mark.parametrize("body", [NO_PRAM_CD_BODY, NO_PRAM_DD_BODY], indirect=True)
+class MissingParam(CommonBaseTests):
     """Test the response message for missing drift parameter."""
 
     def test_error_msg(self, response):
@@ -48,30 +86,13 @@ class MissingParam:
         assert {"_schema": ["Include parameters if drift."]} in errors
 
 
-BAD_DRIFT_1 = {"drift": "str", "parameters": {"p_value": 5}}
-BAD_DRIFT_2 = {"drift": True, "parameters": {}}
-
-
-@mark.parametrize("body", [{"unknown": "val"}], indirect=True)
-class TestBadBodyKey(UnknownField, CommonBaseTests):
+class TestBadBodyKey(UnknownField, IsPrivate, EditGroup, WithDatabase):
     """Test the unknown key parameter in body."""
 
 
-@mark.parametrize("concept_drift", [BAD_DRIFT_1], indirect=True)
-class TestNoCBoolDrift(NoBoolDrift, CommonBaseTests):
+class TestNoBoolDrift(NoBoolDrift, IsPrivate, EditGroup, WithDatabase):
     """Test the response when missing concept drift boolean."""
 
 
-@mark.parametrize("data_drift", [BAD_DRIFT_1], indirect=True)
-class TestNoDBoolDrift(NoBoolDrift, CommonBaseTests):
-    """Test the response when missing data drift boolean."""
-
-
-@mark.parametrize("concept_drift", [BAD_DRIFT_2], indirect=True)
-class TestMissingCParam(MissingParam, CommonBaseTests):
+class TestMissingParam(MissingParam, IsPrivate, EditGroup, WithDatabase):
     """Test the response when missing concept drift parameter."""
-
-
-@mark.parametrize("concept_drift", [BAD_DRIFT_2], indirect=True)
-class TestMissingCDrift(MissingParam, CommonBaseTests):
-    """Test the response when missing data drift parameter."""
