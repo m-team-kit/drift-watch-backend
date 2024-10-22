48b7d3578237e0b962735d527fa12553afae5eb8
Borja Esteban <boressan@outlook.com>
2024-10-22 04:29:59 +0000

    fix tests for experiment put 422 response

--- tests/blp_experiment/experiment/put/test_422.py
+++ tests/blp_experiment/experiment/put/test_422.py
@@ -1,13 +1,11 @@
-"""Testing module for endpoint methods /drift."""
+"""Testing module for endpoint methods /experiment."""
 
 # pylint: disable=redefined-outer-name
 from pytest import mark
 
 
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.usefixtures("accept_authorization")
 class CommonBaseTests:
-    """Common tests for the /drift endpoint."""
+    """Common tests for the /experiment endpoint."""
 
     def test_status_code(self, response):
         """Test the 422 response."""
@@ -15,51 +13,83 @@ class CommonBaseTests:
         assert response.json["code"] == 422
 
 
-class UnknownField:
-    """Test response message contains unknown field."""
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
 
-    def test_error_msg(self, response):
-        """Test message contains useful information."""
-        assert response.json["status"] == "Unprocessable Entity"
-        errors = response.json["errors"]["json"].values()
-        assert ["Unknown field."] in errors
-
-
-class InvalidString:
-    """Test response message contains invalid field."""
-
-    def test_error_msg(self, response):
-        """Test message contains useful information."""
-        assert response.json["status"] == "Unprocessable Entity"
-        errors = response.json["errors"]["json"].values()
-        assert ["Not a valid string."] in errors
+    def test_not_saved(self, response, db_experiment):
+        """Test the response items are in the database."""
+        assert db_experiment is not None
+        assert response.json != db_experiment
 
 
-class InvalidMapping:
-    """Test response message contains invalid list."""
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
 
-    def test_error_msg(self, response):
-        """Test message contains useful information."""
-        errors = response.json["errors"]["json"].values()
-        assert ["Not a valid mapping type."] in errors
+
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
 
 
 EXPERIMENT_1 = "00000000-0000-0001-0001-000000000001"
 
 
 @mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("body", [{"bad_key": "val"}], indirect=True)
-class TestBadBodyKey(UnknownField, CommonBaseTests):
-    """Test the bad_key parameter in the body."""
+class IsPrivate(CommonBaseTests):
+    """Base class for group with public as false."""
 
 
-@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("name", [1000], indirect=True)
-class TestBadExperiment(InvalidString, CommonBaseTests):
-    """Test experiment name is not a string."""
+ENT_MANAGE = "urn:mace:egi.eu:group:vo_example1:role=manage#x.0"
+ENT_EDIT = "urn:mace:egi.eu:group:vo_example1:role=edit#x.0"
+ENT_READ = "urn:mace:egi.eu:group:vo_example1:role=read#x.0"
 
 
-@mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("permissions", ["non_map"], indirect=True)
-class TestBadPermissions(InvalidMapping, CommonBaseTests):
-    """Test permissions is not a list."""
+@mark.parametrize("entitlements", [[ENT_MANAGE]], indirect=True)
+class ManageGroup(Registered):
+    """Base class for group with manage entitlement tests."""
+
+
+@mark.parametrize("body", [{"unknown": "val"}], indirect=True)
+class UnknownField(WithDatabase):
+    """Test the response message for unknown key in body."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"].values()
+        assert ["Unknown field."] in errors
+
+
+@mark.parametrize("public", ["str"], indirect=True)
+class NoBoolPublic(WithDatabase):
+    """Test the response message for missing experiment boolean."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"]
+        assert "Not a valid boolean." in errors["public"]
+
+
+@mark.parametrize("permissions", ["str"], indirect=True)
+class NoListPerm(WithDatabase):
+    """Test the response message for missing experiment parameter."""
+
+    def test_error_msg(self, response):
+        """Test message contains useful information."""
+        errors = response.json["errors"]["json"]
+        assert "Not a valid mapping type." in errors["permissions"]
+
+
+class TestBadBodyKey(UnknownField, IsPrivate, ManageGroup):
+    """Test the unknown key parameter in body."""
+
+
+class TestNoBoolPublic(NoBoolPublic, IsPrivate, ManageGroup):
+    """Test the response when missing concept experiment boolean."""
+
+
+class TestNoListPerm(NoListPerm, IsPrivate, ManageGroup):
+    """Test the response when missing concept experiment parameter."""
