3fba13ccb7227a87677f8a23c81f71964c863d16
Borja Esteban <boressan@outlook.com>
2024-10-22 05:39:18 +0000

    add tests for self/get endpoint

---
+++ tests/blp_user/self/get/test_200.py
@@ -0,0 +1,60 @@
+"""Testing module for endpoint methods /user/self."""
+
+# pylint: disable=redefined-outer-name
+from datetime import datetime as dt
+from uuid import UUID
+
+from pytest import mark
+
+
+class CommonBaseTests:
+    """Common tests for the /user/self endpoint."""
+
+    def test_status_code(self, response):
+        """Test the 200 response."""
+        assert response.status_code == 200
+
+    def test_has_id(self, response):
+        """Test the response items have an id."""
+        assert "id" in response.json
+        assert UUID(response.json["id"])
+
+    def test_has_created_at(self, response):
+        """Test the response items have a created_at."""
+        assert "created_at" in response.json
+        assert dt.fromisoformat(response.json["created_at"])
+
+    def test_has_email(self, response):
+        """Test the response items have a name."""
+        assert response.json["email"] is not None
+
+
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
+
+    def test_in_database(self, response, db_user):
+        """Test the response items are in the database."""
+        assert db_user is not None
+        assert response.json == db_user
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
+
+
+@mark.parametrize("email", ["email@example.com"], indirect=True)
+class ValidEmail(CommonBaseTests):
+    """Tests for message response when user is registered."""
+
+
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
+
+
+class TestSimpleInfo(Registered, ValidEmail, WithDatabase):
+    """Test the response when user is registered."""
