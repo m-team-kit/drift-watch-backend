e6bf2c7815c5bc9ab7b37395eb69c4144d3e91c5
Borja Esteban <boressan@outlook.com>
2024-10-22 04:29:48 +0000

    fix tests for experiment put 403 response

--- tests/blp_experiment/experiment/put/test_403.py
+++ tests/blp_experiment/experiment/put/test_403.py
@@ -4,11 +4,6 @@
 from pytest import mark
 
 
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.parametrize("name", ["experiment_a"], indirect=True)
-@mark.parametrize("with_database", ["database_1"], indirect=True)
-@mark.usefixtures("with_context", "with_database")
-@mark.usefixtures("accept_authorization")
 class CommonBaseTests:
     """Common tests for the /experiment endpoint."""
 
@@ -18,7 +13,20 @@ class CommonBaseTests:
         assert response.json["code"] == 403
 
 
-class NotRegistered:
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
+
+
+@mark.parametrize("subiss", [("unknown", "issuer.1")], indirect=True)
+class NotRegistered(ValidAuth):
     """Tests for message response when user is not registered."""
 
     def test_error_msg(self, response):
@@ -27,7 +35,12 @@ class NotRegistered:
         assert response.json["message"] == "User not registered."
 
 
-class PermissionDenied:
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
+
+
+class PermissionDenied(Registered):
     """Tests for message response when user does not have permission."""
 
     def test_error_msg(self, response):
@@ -41,22 +54,33 @@ EXPERIMENT_2 = "00000000-0000-0001-0001-000000000002"
 
 
 @mark.parametrize("experiment_id", [EXPERIMENT_1], indirect=True)
-@mark.parametrize("subiss", [("unknown", "egi.com")], indirect=True)
-class TestNotRegistered(NotRegistered, CommonBaseTests):
+class IsPrivate(CommonBaseTests):
+    """Base class for group with public as false."""
+
+
+@mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
+class IsPublic(CommonBaseTests):
+    """Base class for group with public as true."""
+
+
+ENT_MANAGE = "urn:mace:egi.eu:group:vo_example1:role=manage#x.0"
+ENT_EDIT = "urn:mace:egi.eu:group:vo_example1:role=edit#x.0"
+ENT_READ = "urn:mace:egi.eu:group:vo_example1:role=read#x.0"
+GROUPS_WITHOUT_MANAGE = [[ENT_EDIT], [ENT_READ], []]
+
+
+@mark.parametrize("entitlements", GROUPS_WITHOUT_MANAGE, indirect=True)
+class NoManage(PermissionDenied):
+    """Base class for group without entitlement tests."""
+
+
+class TestNotRegistered(NotRegistered, IsPublic, WithDatabase):
     """Test the authentication response when user not registered."""
 
 
-GROUP_1 = "urn:mace:egi.eu:group:vo_example1:role=group1#aai.egi.eu"
-PERMISSIONS = {GROUP_1: "Read"}
+class TestNoAccessPublic(NoManage, IsPublic, WithDatabase):
+    """Tests for message response for no permission."""
 
 
-@mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
-@mark.parametrize("permissions", [{}], indirect=True)
-class TestNoPermissions(PermissionDenied, CommonBaseTests):
-    """Tests for message response when user does not have manage permission."""
-
-
-@mark.parametrize("experiment_id", [EXPERIMENT_2], indirect=True)
-@mark.parametrize("permissions", [PERMISSIONS], indirect=True)
-class TestBadEntitlements(PermissionDenied, CommonBaseTests):
-    """Tests for message response when mismatch user's with new entitlements."""
+class TestNoAccessPrivate(NoManage, IsPrivate, WithDatabase):
+    """Tests for message response for no permission."""
