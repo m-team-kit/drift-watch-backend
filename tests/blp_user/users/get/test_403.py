2076eab6ef1f8a5fb8181c5f7226a5c3463216ec
Borja Esteban <boressan@outlook.com>
2024-10-22 06:03:38 +0000

    fix tests for users get 403 response

--- tests/blp_user/users/get/test_403.py
+++ tests/blp_user/users/get/test_403.py
@@ -4,12 +4,8 @@
 from pytest import mark
 
 
-@mark.parametrize("auth", ["mock-token"], indirect=True)
-@mark.parametrize("with_database", ["database_1"], indirect=True)
-@mark.usefixtures("with_context", "with_database")
-@mark.usefixtures("accept_authorization")
 class CommonBaseTests:
-    """Common tests for the /drift endpoint."""
+    """Common tests for the /users endpoint."""
 
     def test_status_code(self, response):
         """Test the 403 response."""
@@ -17,17 +13,41 @@ class CommonBaseTests:
         assert response.json["code"] == 403
 
 
+@mark.parametrize("with_database", ["database_1"], indirect=True)
+@mark.usefixtures("with_context", "with_database")
+class WithDatabase(CommonBaseTests):
+    """Base class for tests using database."""
+
+
+@mark.parametrize("auth", ["mock-token"], indirect=True)
+@mark.usefixtures("accept_authorization")
+class ValidAuth(CommonBaseTests):
+    """Base class for valid authenticated tests."""
+
+
+@mark.parametrize("subiss", [("user_4", "issuer.1")], indirect=True)
+class Registered(ValidAuth):
+    """Tests for message response when user is  registered."""
+
+
+@mark.parametrize("subiss", [("unknown", "issuer.1")], indirect=True)
+class NotRegistered(ValidAuth):
+    """Tests for message response when user is not registered."""
+
+
+@mark.parametrize("entitlements", [None], indirect=True)
 class NotAdmin:
     """Tests for message response when user is not admin."""
 
     def test_error_msg(self, response):
         """Test message contains useful information."""
         assert response.json["status"] == "Forbidden"
-        assert (
-            response.json["message"]
-            == "User user_1@issuer.1 does not meet requirements"
-        )
+        assert "does not meet requirements" in response.json["message"]
 
 
-class TestPermission(NotAdmin, CommonBaseTests):
+class TestRegisteredUser(Registered, NotAdmin, CommonBaseTests):
+    """Tests for message response when user has not the entitlements."""
+
+
+class TestUnknownUser(NotRegistered, NotAdmin, CommonBaseTests):
     """Tests for message response when user has not the entitlements."""
